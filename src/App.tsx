33262e619ae3018c708e5d40d43305bf87583d6c
fujisii <51264835+fujisii@users.noreply.github.com>
2022-05-11 12:33:18 +0900

    update: counter

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p className="TotalCount">0</p>
-        <button type="button" className="PlusButton" onClick={countUp}>+</button>
-        <button type="button" className="MinusButton" onClick={countDown}>-</button>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-function countUp() {
-  console.log('count up');
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button type="button" onClick={() => setCount(count + 1)}>+</button>
+      <h1>{count}</h1>
+      <button type="button" onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  )
 }
-function countDown() {
-  console.log('count down');
-}
-// TODO: https://typescriptbook.jp/tutorials/react-like-button-tutorial
-// TODO: keyword: react counter codepen
-//        https://codepen.io/x-cold/pen/ZNEReY
 
 export default App;
