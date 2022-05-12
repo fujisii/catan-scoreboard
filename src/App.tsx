ed19560e6662a16ea5aa84db2d5d1ad6612ffc81
fujisii <51264835+fujisii@users.noreply.github.com>
2022-05-12 12:46:56 +0900

    update: counter and css

--- src/App.tsx
+++ src/App.tsx
@@ -53,9 +53,19 @@ function Counter() {
   const [count, setCount] = React.useState(0);
   return (
     <div>
-      <button type="button" onClick={() => setCount(count + 1)}>+</button>
-      <h1>{count}</h1>
-      <button type="button" onClick={() => setCount(count - 1)}>-</button>
+      <p className="TotalCount">0</p>
+      <div className="Settlement">
+        <p>開拓地</p>
+        <button type="button" onClick={() => setCount(count + 1)}>+</button>
+        <p>{count}</p>
+        <button type="button" onClick={() => setCount(count - 1)}>-</button>
+      </div>
+      <div className="City">
+        <p>都市</p>
+        <button type="button" onClick={() => setCount(count + 1)}>+</button>
+        <p>{count}</p>
+        <button type="button" onClick={() => setCount(count - 1)}>-</button>
+      </div>
     </div>
   )
 }
