aad95048f55f4058c6039a8b0f840b46d20cb615
fujisii <51264835+fujisii@users.noreply.github.com>
2022-05-13 23:33:31 +0900

    add: images

--- src/components/atoms/counter.tsx
+++ src/components/atoms/counter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import settlement from '../../images/settlement.svg';
 
 function Counter() {
   const [count, setCount] = React.useState(0);
@@ -6,7 +7,7 @@ function Counter() {
     <div>
       <p className="TotalCount">0</p>
       <div className="Settlement">
-        <p>開拓地</p>
+        <img src={settlement} alt="settlement" />
         <button type="button" onClick={() => setCount(count + 1)}>+</button>
         <p>{count}</p>
         <button type="button" onClick={() => setCount(count - 1)}>-</button>
@@ -19,6 +20,18 @@ function Counter() {
       </div>
       <div className="Road">
         <p>街道</p>
+        <button type="button" onClick={() => setCount(count + 1)}>+</button>
+        <p>{count}</p>
+        <button type="button" onClick={() => setCount(count - 1)}>-</button>
+      </div>
+      <div className="Knight">
+        <p>騎士</p>
+        <button type="button" onClick={() => setCount(count + 1)}>+</button>
+        <p>{count}</p>
+        <button type="button" onClick={() => setCount(count - 1)}>-</button>
+      </div>
+      <div className="VictoryPoint">
+        <p>勝利ポイント</p>
         <button type="button" onClick={() => setCount(count + 1)}>+</button>
         <p>{count}</p>
         <button type="button" onClick={() => setCount(count - 1)}>-</button>
