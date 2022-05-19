5d834d873b943201c223e612c5aa6e9a761ce77a
fujisii <51264835+fujisii@users.noreply.github.com>
2022-05-19 12:53:12 +0900

    add: counter useState

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import Template from 'components/templates/template';
 
 // eslint-disable-next-line react/function-component-definition
 function App() {
+  const counter = {
+    settlement: 0,
+    city: 0,
+    road: 0,
+    knight: 0,
+    victoryPoint: 0
+  };
+  const [count, setCount] = React.useState({
+    red: counter,
+    blue: counter
+  });
+
   return (
     <div className="App">
       <Template />
