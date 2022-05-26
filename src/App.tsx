bbe73739e552364bab630c5c2418c57ad5195d0b
fujisii <51264835+fujisii@users.noreply.github.com>
2022-05-26 12:48:39 +0900

    update: context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import Template from 'components/templates/template';
+import Counter from 'components/atoms/counter';
 
-const total = {
+type totalContext = {
+  totalCount: number;
+  setTotalCount: () => void;
+}
+
+const defaultContext: totalContext = {
   totalCount: 0,
-  setTotalCount: () => {
-    console.log('test');
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setTotalCount: () => {}
 };
 
-export const TotalCount = React.createContext(total);
+export const TotalCount = React.createContext<totalContext>(defaultContext);
+
+export const useTotalCount = (): totalContext => {
+  const [count, setCount] = useState(0);
+
+  const setTotalCount = useCallback((): void => {
+    setCount(count + 1);
+  }, []);
+
+  return {
+    count, // TODO
+    setTotalCount
+  };
+};
 
 // eslint-disable-next-line react/function-component-definition
 function App() {
