e1930f81bc3eeb3be854f163c1dec838abb4be83
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-02 12:28:32 +0900

    add: special card useState

--- src/components/molecules/special.tsx
+++ src/components/molecules/special.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import road from 'images/road.svg';
 import knight from 'images/knight.svg';
 
 function Special() {
+  const [largestArmy, setIsLargestArmy] = useState(false);
+  const [longestRoad, setIsLongestRoad] = useState(false);
+
   return (
     <div className="Special SideBySide">
       <img src={road} alt="road" />
