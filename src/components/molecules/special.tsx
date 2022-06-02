7404f3ef7d101ebcda9c2c94fa0081488f7a518e
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-02 12:41:06 +0900

    add: toggle special

--- src/components/molecules/special.tsx
+++ src/components/molecules/special.tsx
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import road from 'images/road.svg';
 import knight from 'images/knight.svg';
 
+import { TotalCount } from 'components/molecules/area'
+
 function Special() {
-  const [largestArmy, setIsLargestArmy] = useState(false);
-  const [longestRoad, setIsLongestRoad] = useState(false);
+  const [largestArmy, setIsLargestArmy] = useState(true);
+  const [longestRoad, setIsLongestRoad] = useState(true);
+
+  const ctx = useContext(TotalCount);
+
+  const toggleLargestArmy = () => {
+    setIsLargestArmy(!largestArmy);
+
+    if (largestArmy) {
+      ctx.setTotalCount(2);
+    } else {
+      ctx.downTotalCount(2);
+    }
+  }
+
+  const toggleLongestRoad = () => {
+    setIsLongestRoad(!longestRoad);
+
+    if (longestRoad) {
+      ctx.setTotalCount(2);
+    } else {
+      ctx.downTotalCount(2);
+    }
+  }
 
   return (
     <div className="Special SideBySide">
-      <img src={road} alt="road" />
-      <img src={knight} alt="knight" />
+      <div onClick={toggleLongestRoad} aria-hidden="true">
+        <img src={road} alt="road" />
+      </div>
+      <div onClick={toggleLargestArmy} aria-hidden="true">
+        <img src={knight} alt="knight" />
+      </div>
     </div>
   )
 }
