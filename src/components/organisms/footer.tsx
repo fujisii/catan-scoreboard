c3c6057d6ee2313117fa8223eb775941a32052c3
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-08 07:50:20 +0900

    update:フッターの作成者リンクを別タブで開くようにした

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
       <div className="Footer">
-        Created by <a href="https://github.com/fujisii">fujisii</a>
+        Created by <a href="https://github.com/fujisii" target="_blank" rel="noopener noreferrer">fujisii</a>
       </div>
     );
   }
