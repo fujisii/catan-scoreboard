bb7fe69c7dd262124c76fa04c5da3a5ed493b955
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-10 12:40:37 +0900

    update: スクロールを抑制

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import Template from 'components/templates/template';
 
 // eslint-disable-next-line react/function-component-definition
 function App() {
-  const setVh = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
+  // const setVh = () => {
+  //   const vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // };
 
-  window.addEventListener('load', setVh);
-  window.addEventListener('resize', setVh);
+  // window.addEventListener('load', setVh);
+  // window.addEventListener('resize', setVh);
+
+  // iOSスクロール抑制
+  const disableScroll = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+  }
+  document.addEventListener('touchmove', disableScroll, { passive: false });
 
   return (
     <div className="App">
