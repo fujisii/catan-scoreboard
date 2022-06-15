371e197f47d7b35c0262cd173d336c59f1a25d48
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-15 12:55:32 +0900

    upadte: css クラス名変更（カウンター）

--- src/components/atoms/counter.tsx
+++ src/components/atoms/counter.tsx
@@ -16,7 +16,7 @@ function Counter({point}:{point: number}) {
   }
 
   return (
-    <div className="Counter side-by-side">
+    <div className="counter side-by-side">
       <button type="button" onClick={countUp}>+</button>
       <p>{count}</p>
       <button type="button" onClick={countDown}>-</button>
