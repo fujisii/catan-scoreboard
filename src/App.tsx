c61d48c5b207e8fc1cd7f6a8a4f3e83318ae0683
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-17 12:15:51 +0900

    update: 不要なコメントを削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 import './App.css';
 import Template from 'components/templates/template';
 
-// eslint-disable-next-line react/function-component-definition
 function App() {
-  // const setVh = () => {
-  //   const vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // };
-
-  // window.addEventListener('load', setVh);
-  // window.addEventListener('resize', setVh);
-
   // iOSスクロール抑制
   const disableScroll = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
