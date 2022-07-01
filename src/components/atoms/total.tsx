567bd17c547c88b1f3e00d8dbdae5a4d58962dbf
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-01 12:31:04 +0900

    add: 10ポイント先取で紙吹雪が舞う

--- src/components/atoms/total.tsx
+++ src/components/atoms/total.tsx
@@ -1,12 +1,20 @@
 import React, { useContext } from 'react';
 
-import { TotalCount } from 'components/molecules/area'
+import { TotalCount } from 'components/molecules/area';
+
+import Winner from 'components/atoms/winner';
 
 function Total() {
   const totalCount = useContext(TotalCount);
+
+  let winner;
+  if (totalCount.totalCount === 10) {
+    winner = <Winner />;
+  }
   return (
     <div className="total">
       {totalCount.totalCount}
+      {winner}
     </div>
   )
 }
---
+++ src/components/atoms/winner.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+
+function Winner() {
+    // 要素の幅, 高さ
+    const { width, height } = useWindowSize();
+    // 重力
+    const gravity = 0.1;
+    // 一度に放出される紙吹雪の数
+    const numberOfPieces = 2000;
+    // 紙吹雪がスポーンする長方形。デフォルトは上部にあります。
+    const confettiSource = {
+        x: 0,
+        y: 0,
+        w: width,
+        h: height
+    };
+    const recycle = false;
+    // 紙吹雪が水平方向に放出される値の範囲。正の数は右向き、負の数は左向きです。
+    const initialVelocityX = { min: 5, max: 5 };
+    // 紙吹雪が垂直に放出される値の範囲。正の数は下向き、負の数は上向きです。
+    const initialVelocityY = { min: 20, max: -20 };
+
+    return (
+        <Confetti
+            width={width}
+            height={height}
+            numberOfPieces={numberOfPieces}
+            confettiSource={confettiSource}
+            recycle={recycle}
+            gravity={gravity}
+            // initialVelocityX={initialVelocityX}
+            // initialVelocityY={initialVelocityY}
+        />
+    );
+}
+
+export default Winner;
